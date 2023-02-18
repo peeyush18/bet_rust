4515293d96fbac2129b3a88a4bf4ef1abee5bd8b
Ishita Singh <ishita.singh410@gmail.com>
2023-02-18 11:27:22 +0100

    adding hashmap functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use radix_trie::Trie;
+use std::collections::HashMap;
 
 pub fn init_trie() -> Trie<String, i32> {
     return Trie::new();
@@ -9,5 +10,17 @@ pub fn add_word_to_trie(word: String, line_num: i32, store: &mut Trie<String, i3
 }
 
 pub fn search_word_line_num(word: &String, store: & Trie<String, i32>) -> Option<i32> {
+    return store.get(word).copied()
+}
+
+pub fn init_hash_map() -> HashMap<String, i32> {
+    return HashMap::new();
+}
+
+pub fn add_word_to_hash_map(word: String, line_num: i32, store: &mut HashMap<String, i32>) {
+    store.insert(word, line_num);
+}
+
+pub fn search_fast_word_line_num(word: &String, store: & HashMap<String, i32>) -> Option<i32> {
     return store.get(word).copied()
 }
