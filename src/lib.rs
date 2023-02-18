fbe14da75d231994c9d7504d568716df5f26a2bd
istio <peeyush.pathak18@gmail.com>
2023-02-18 11:20:49 +0100

    adding radix tree for trie

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+use radix_trie::Trie;
+
+pub fn init_trie() -> Trie<String, i32> {
+    return Trie::new();
+}
+
+pub fn add_word_to_trie(word: String, line_num: i32, store: &mut Trie<String, i32>) {
+    store.insert(word, line_num);
+}
+
+pub fn search_word_line_num(word: &String, store: & Trie<String, i32>) -> Option<&i32> {
+    return store.get(word)
+}
